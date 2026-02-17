85c2711ee3b20c140ff1f85d9f3d776a2ff838c6
Daniel <89817884+daniel-lzyn@users.noreply.github.com>
2026-02-17 14:57:35 +0800

    Added initial webpack setup.

---
+++ webpack.common.js
@@ -0,0 +1,19 @@
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import path from "node:path";
+
+export default {
+    entry: {
+        app: './src/index.js',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/index.html",
+            title: 'Production',
+        }),
+    ],
+    output: {
+        filename: 'main.bundle.js',
+        path: path.resolve(import.meta.dirname, 'dist'),
+        clean: true,
+    },
+};
