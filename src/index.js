d08cb2d90217d151ee92a67937a4bc6694c5544a
Daniel <89817884+daniel-lzyn@users.noreply.github.com>
2026-02-22 16:00:26 +0800

    Added function to open and close form dialog.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,31 @@
-alert('test')
+import './style.css'
+
+// Add project form
+const addProjectBtn = document.getElementById('add_project_btn');
+const projectDialog = document.getElementById('project_dialog');
+const projectCloseForm = document.getElementById('project_close_form')
+
+// Add todo item form
+const addTodoBtn = document.getElementById('add_todo_btn');
+const todoDialog = document.getElementById('todo_dialog');
+const todoCloseForm = document.getElementById('todo_close_form')
+
+// Function to display dialog
+function displayDialog(btn, dialog) {
+    btn.addEventListener('click', () => {
+        dialog.showModal();
+    });
+};
+
+// Function to close dialog
+function closeDialog(btn, dialog) {
+    btn.addEventListener('click', () => {
+        dialog.close();
+    });
+}
+
+displayDialog(addProjectBtn, projectDialog);
+closeDialog(projectCloseForm, projectDialog);
+
+displayDialog(addTodoBtn, todoDialog);
+closeDialog(todoCloseForm, todoDialog);
