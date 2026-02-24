e1b4498e501ed8e8bdb532b0dfa490b9d5361ee9
Daniel <89817884+daniel-lzyn@users.noreply.github.com>
2026-02-24 15:18:24 +0800

    Added function to delete todo items.

--- src/index.js
+++ src/index.js
@@ -26,6 +26,13 @@ const todoDialog = document.getElementById('todo_dialog');
 const todoCloseForm = document.getElementById('todo_close_form');
 const todoForm = document.getElementById('todo_form');
 
+// Displaying and closing dialog
+displayDialog(addProjectBtn, projectDialog);
+closeDialog(projectCloseForm, projectDialog);
+
+displayDialog(addTodoBtn, todoDialog);
+closeDialog(todoCloseForm, todoDialog);
+
 // Function to store project object.
 submitForm(projectForm);
 
@@ -72,13 +79,6 @@ const todoStoreItem = function(formObject) {
 
 formObserve(todoStoreItem);
 
-// Displaying and closing dialog
-displayDialog(addProjectBtn, projectDialog);
-closeDialog(projectCloseForm, projectDialog);
-
-displayDialog(addTodoBtn, todoDialog);
-closeDialog(todoCloseForm, todoDialog);
-
 // Function to refresh todo list
 function refreshTodo() {
     const todoContainer = document.getElementById('todo_container');
@@ -87,25 +87,49 @@ function refreshTodo() {
 
     if (selectedProject in projectContainer) {
         const currentTodo = projectContainer[selectedProject];
-        const todoDiv = document.createElement('div');
 
         todoTitle.textContent = `${currentTodo.projectTitle} todo list:`;
         currentTodo.todoList.forEach(todoItem => {
+            const todoDiv = document.createElement('div');
+
             const todoName = document.createElement('p');
             const todoDesc = document.createElement('p');
             const todoDueDate = document.createElement('p');
             const todoPrioroty = document.createElement('p');
+            const deleteBtn = document.createElement('button');
 
             todoName.textContent = todoItem.title;
             todoDesc.textContent = `Description: ${todoItem.desc}`;
             todoDueDate.textContent = `Due date: ${todoItem.dueDate}`;
             todoPrioroty.textContent = `Priority: ${todoItem.priority}`;
+            deleteBtn.textContent = "Delete";
 
-            todoDiv.append(todoName, todoDesc, todoDueDate, todoPrioroty);
+            todoDiv.classList.add(todoName.textContent);
+            todoDiv.append(todoName, todoDesc, todoDueDate, todoPrioroty, deleteBtn);
             todoContainer.append(todoDiv);
         });
     };
 };
+
+(function() {
+    const todoContainer = document.getElementById('todo_container');
+    todoContainer.addEventListener('click', (e) => {
+        let selectedTodoItem = "";
+
+        if(e.target.textContent === "Delete") {
+            selectedTodoItem = e.target.parentNode.classList[0];
+        };
+
+        if(selectedTodoItem !== "") {
+            const todoIndex = projectContainer[selectedProject].todoList.findIndex(prop => prop.title === selectedTodoItem);
+            console.log(selectedTodoItem);
+            console.log(projectContainer[selectedProject].todoList)
+            console.log(todoIndex);
+            delete projectContainer[selectedProject].todoList[todoIndex];
+            refreshTodo();
+        };
+    });
+})();
 
 // Adding event listener to projects.
 const projectContainerEl = document.getElementById('project_container');
